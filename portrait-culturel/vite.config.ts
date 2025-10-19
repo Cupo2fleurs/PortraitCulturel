83df0ac8e6b1ce5b1cc1320e10516988a19a4625
cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2025-10-19 20:16:30 +0200

    Refactor Vite config to improve readability

--- portrait-culturel/vite.config.ts
+++ portrait-culturel/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [
     react({
       babel: {
@@ -9,5 +10,6 @@ export default defineConfig(({ command }) => ({
       },
     }),
   ],
-  base:'',
-}))
+  base: '',
+})
+
